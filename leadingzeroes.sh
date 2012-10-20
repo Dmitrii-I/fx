25f8a7078ad9ce5e34195833fd6303cfbe931d0a
Dmitrii Izgurskii <izgurskii@gmail.com>
2012-10-20 19:19:40 +0200

    update

--- leadingzeroes.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# This script applies leadingzeroes.awk script on each file in a directory
-cd ~/oanda/1/
-for a in *; do
-	if [ -f "$a" ]; then # was it a file?
-		cat $a | awk -f ~/Documents/leadingzeroes.awk > ~/oanda/1/clean/$a
-	fi
-done
-
